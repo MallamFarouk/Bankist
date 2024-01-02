556e46ddfbe5de728414c444ed248ac8bf6f62c0
MallamFarouk <farukissa010@gmail.com>
2024-01-01 16:53:17 -0800

    New Year Commit()

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,30 @@ const restaurant = {
     },
   },
 };
+
+// orderPizza(mainIngredient, ...otherIngredients){
+//   console.log(mainIngredient)
+//   console.log(otherIngredients)
+// };
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  [4, 'Python'],
+  ['correct', 3],
+  [true, 'correct🍕🍕'],
+  [false, 'Try again please🙄😶']
+]);
+console.log(question)
+console.log(Object.entries(restaurant.openingHours))
+const hoursMap = new Map(Object.entries(restaurant.openingHours));
+console.log(hoursMap)
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`)
+}
+
+const answer = Number(prompt('Your Answer'));
+console.log(answer)
+console.log(question.get(question.get('correct') === answer));
